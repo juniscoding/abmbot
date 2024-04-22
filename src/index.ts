f529c12aae2213c7764194f31f3878729c2437b8
Valentina Nakayama <valentina@poli.ufrj.br>
2024-04-22 11:17:50 -0300

    bible bot v1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
-const text = await Bot.run(getPostText, { dryRun: true });
+const text = await Bot.run(getPostText, { dryRun: false });
 
 console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+
+// console.log(getPostText)
--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,12 @@
+import verses from "./verses.json" assert { type: "json" };
+
+
 export default async function getPostText() {
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  return "Hello from the Bluesky API";
+  // const verses = await ()
+  // const verses = require('./verses.json');
+  // const versesArray: { verses: array }[] = verses;
+
+  var random = verses.verses[Math.floor(Math.random() * Object.keys(verses.verses).length)];
+  return random;
 }
